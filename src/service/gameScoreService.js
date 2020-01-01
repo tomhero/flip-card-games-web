4731aa01416faf9dd3f9b4aacef53b2e49f7473e
Chayut Ruksomya <chayut.191@gmail.com>
2020-01-01 20:20:41 +0700

    Try to use axios

--- src/service/gameScoreService.js
+++ src/service/gameScoreService.js
@@ -1,21 +1,17 @@
 /* eslint-disable no-console */
+import axios from 'axios'
+
 const host = process.env.VUE_APP_API_HOST || '35.227.148.145'
 const port = process.env.VUE_APP_API_PORT
 const apiKey = process.env.VUE_APP_API_KEY || 'be511834-5b8c-480f-98f0-8b741d3a4d65'
 const apiVersion = process.env.VUE_APP_API_VERSION || 'v1'
 
-const apiUrl = `https://${host}${port ? ':'+port : ''}/api/${apiVersion}/highscore/${apiKey}`
+const apiUrl = `http://${host}${port ? ':' + port : ''}/api/${apiVersion}/highscore/${apiKey}`
 
-export function getGlobelHighestScore() {
-  return fetch(apiUrl, { mode: 'cors' }).then(response => response.json())
+export function getGlobelHighestScore () {
+  return axios.get(apiUrl)
 }
 
-export function sendPlayerHighScore(score) {
-  return fetch(apiUrl, {
-    method: 'POST',
-    mode: 'cors',
-    body: JSON.stringify({
-      playerHighscore: score
-    })
-  }).then(response => response.json())
+export function sendPlayerHighScore (score) {
+  return axios.post(apiUrl, { playerHighscore: score })
 }
