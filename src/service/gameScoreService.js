bf905cc986f568040b62a3dfc1f93fc43e307cee
Chayut Ruksomya <chayut.191@gmail.com>
2020-01-01 20:33:06 +0700

    #1 Fix Plese specific http/https protocal in .env

--- src/service/gameScoreService.js
+++ src/service/gameScoreService.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 import axios from 'axios'
 
-const host = process.env.VUE_APP_API_HOST || '35.227.148.145'
+const host = process.env.VUE_APP_API_HOST || 'https://35.227.148.145'
 const port = process.env.VUE_APP_API_PORT
 const apiKey = process.env.VUE_APP_API_KEY || 'be511834-5b8c-480f-98f0-8b741d3a4d65'
 const apiVersion = process.env.VUE_APP_API_VERSION || 'v1'
 
-const apiUrl = `http://${host}${port ? ':' + port : ''}/api/${apiVersion}/highscore/${apiKey}`
+const apiUrl = `${host}${port ? ':' + port : ''}/api/${apiVersion}/highscore/${apiKey}`
 
 export function getGlobelHighestScore () {
   return axios.get(apiUrl)
